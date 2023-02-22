53c0573128febc3b27209e366b5e2ee70a90ea6d
hoseok <shsshs0125@naver.com>
2023-02-22 14:30:46 +0900

    Feat: 로그인 기능 완료 및 sha256 암호화 처리 완료

--- src/main/java/shop/mtcoding/job/service/LoginService.java
+++ src/main/java/shop/mtcoding/job/service/LoginService.java
@@ -1,13 +1,15 @@
 package shop.mtcoding.job.service;
 
+import java.security.NoSuchAlgorithmException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import shop.mtcoding.job.dto.user.UserReqDto.LoginReqDto;
-import shop.mtcoding.job.handler.exception.CustomException;
 import shop.mtcoding.job.model.user.User;
 import shop.mtcoding.job.model.user.UserRepository;
+import shop.mtcoding.job.util.HashEncoding;
 
 @Service
 public class LoginService {
@@ -16,14 +18,14 @@ public class LoginService {
 
     @Transactional(readOnly = true)
     public User 로그인하기(LoginReqDto loginReqDto) {
-        // 1. db에 select하기
-        User principal = userRepository.findByUsernameAndPassword(loginReqDto.getUsername(), loginReqDto.getPassword());
-
-        // 2. principal 유효성 검사
-        if (principal == null) {
-            throw new CustomException("존재하지 않는 아이디거나 비밀번호를 다시 확인해주시기 바랍니다");
+        try {
+            String sha256Hash = HashEncoding.sha256(loginReqDto.getPassword());
+            User principal = userRepository.findByUsernameAndPassword(loginReqDto.getUsername(),
+                    sha256Hash);
+            return principal;
+        } catch (NoSuchAlgorithmException e) {
+            System.err.println("알고리즘을 찾을 수 없습니다: " + e.getMessage());
         }
-
-        return principal;
+        return null;
     }
 }
