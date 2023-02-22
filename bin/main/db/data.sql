0bc71159493f0d7096156f02ccdb69d323ecf1f6
kim ho hyeon <khh5762@naver.com>
2023-02-22 16:40:29 +0900

    Feat: 회원가입 암호화된 password 넣어서 test 완료

--- bin/main/db/data.sql
+++ bin/main/db/data.sql
@@ -1,4 +1,4 @@
 insert into user_tb(username, password, name, email, contact, created_at) values('ssar', '1234', '동식', 'ssar@nate.com', '01011112222', now());
-insert into user_tb(username, password, name, email, contact, created_at) values('cos', '1234', '영식', 'cos@nate.com', '01022223333', now());
+insert into user_tb(username, password, name, email, contact, created_at) values('cos', 'd48ff4b2f68a10fd7c86f185a6ccede0dc0f2c48538d697cb33b6ada3f1e85db', '영식', 'cos@nate.com', '01022223333', now());
 
 commit;
---
+++ src/test/java/shop/mtcoding/job/JoinContorollerTest.java
@@ -0,0 +1,48 @@
+package shop.mtcoding.job;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import javax.servlet.http.HttpSession;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.transaction.annotation.Transactional;
+
+
+@Transactional // 메서드 실행 직후 롤백! // auto_increment 초기화 안 됨.
+@AutoConfigureMockMvc
+@SpringBootTest(webEnvironment = WebEnvironment.MOCK)
+public class JoinContorollerTest {
+    
+    @Autowired
+    private MockMvc mvc;
+
+    @Autowired
+    private HttpSession session;
+
+
+    @Test
+    public void join_test() throws Exception {
+
+        // given
+        String requestBody = "username=cos&password=d48ff4b2f68a10fd7c86f185a6ccede0dc0f2c48538d697cb33b6ada3f1e85db&email=cos@nate.com";
+        // when
+
+        assertNotNull(requestBody);
+        assertFalse(requestBody.isEmpty());
+
+        ResultActions resultActions = mvc.perform(post("/join").content(requestBody)
+                .contentType(MediaType.APPLICATION_FORM_URLENCODED_VALUE));
+        // then
+        resultActions.andExpect(status().is3xxRedirection());
+    }
+}
