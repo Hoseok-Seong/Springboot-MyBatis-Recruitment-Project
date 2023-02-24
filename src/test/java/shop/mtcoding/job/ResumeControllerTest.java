ba1351af05ed8a4d322ba16cdbb36c73c7c06b8b
kim ho hyeon <khh5762@naver.com>
2023-02-24 20:20:48 +0900

    이력서 업데이트 test 완료

--- src/test/java/shop/mtcoding/job/ResumeControllerTest.java
+++ src/test/java/shop/mtcoding/job/ResumeControllerTest.java
@@ -2,6 +2,8 @@ package shop.mtcoding.job;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import java.util.List;
@@ -13,12 +15,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockHttpSession;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 
+import shop.mtcoding.job.dto.resume.ResumeReqDto.ResumeSaveReqDto;
 import shop.mtcoding.job.model.resume.Resume;
 
 @Transactional // 메서드 실행 직후에 롤백!
@@ -30,6 +35,7 @@ public class ResumeControllerTest {
     private MockMvc mvc;
     @Autowired
     private ObjectMapper om;
+    private MockHttpSession mockSession;
     
     @Test
     public void resumeList_test() throws Exception {
@@ -51,5 +57,36 @@ public class ResumeControllerTest {
         assertThat(resumeList.get(0).getUserId()).isEqualTo(1);
         assertThat(resumeList.get(0).getTitle()).isEqualTo("제목1");
 
+    }
+
+    @Test
+    public void update_test() throws Exception {
+        // given
+        ResumeSaveReqDto resumeSaveReqDto = new ResumeSaveReqDto();
+        resumeSaveReqDto.setTitle("제목1-수정");
+        resumeSaveReqDto.setContent("내용1-수정");
+        resumeSaveReqDto.setCareer("경력1-수정");
+        resumeSaveReqDto.setSkill("기술1-수정");
+        resumeSaveReqDto.setAward("수상1-수정");
+        resumeSaveReqDto.setAddress("주소1-수정");
+        resumeSaveReqDto.setLink("링크1-수정");
+        resumeSaveReqDto.setEducation("학력1-수정");
+        resumeSaveReqDto.setFile("파일1-수정");
+        resumeSaveReqDto.setBirthdate("날짜1-수정");
+        resumeSaveReqDto.setLanguage("외국어1-수정");
+
+        String requestBody = om.writeValueAsString(resumeSaveReqDto);
+        System.out.println("테스트 : " + requestBody);
+
+        // when
+        ResultActions resultActions = mvc.perform(
+                put("/resume/1/update")
+                        .content(requestBody)
+                        .contentType(MediaType.APPLICATION_JSON_VALUE)
+                        .session(mockSession));
+
+        // then
+        resultActions.andExpect(status().isOk());
+        resultActions.andExpect(jsonPath("$.code").value(1));
     }
 }
