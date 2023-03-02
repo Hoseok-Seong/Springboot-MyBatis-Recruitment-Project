1e596af1a64ff2fc7ee5a7a05f0ac49d62530797
khh5762 <khh5762@naver.com>
2023-03-02 17:55:29 +0900

    Feat: 매칭 세팅 및 쿼리  완성

--- src/main/java/shop/mtcoding/job/controller/MainController.java
+++ src/main/java/shop/mtcoding/job/controller/MainController.java
@@ -1,12 +1,34 @@
 package shop.mtcoding.job.controller;
 
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import shop.mtcoding.job.dto.user.UserMatchingDto;
+import shop.mtcoding.job.handler.exception.CustomException;
+import shop.mtcoding.job.model.user.User;
+import shop.mtcoding.job.model.user.UserRepository;
 
 @Controller
 public class MainController {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private HttpSession session;
+
     @GetMapping({ "/", "/main" })
-    public String main() {
+    public String main(Model model) {
+        User principal = (User) session.getAttribute("principal");
+        if (principal != null) {
+            UserMatchingDto userMatchingDto = userRepository.userMatching(principal.getId());
+            model.addAttribute("userMatching", userMatchingDto);
+        }
         return "/main/main";
     }
 }
---
+++ src/main/java/shop/mtcoding/job/dto/user/UserMatchingDto.java
@@ -0,0 +1,12 @@
+package shop.mtcoding.job.dto.user;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+public class UserMatchingDto {
+    private int userId;
+    private String enterpriseName;
+    private String enterpriseLogo;
+}
